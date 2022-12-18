e69f2967a96d695f664af31728651d4430db8524
RiperFish <mustapha.benfetta@gmail.com>
2022-12-18 12:08:56 +0100

    Show the visitor different information depending on the availability of an operator

--- src/op_status.php
+++ src/op_status.php
@@ -12,25 +12,3 @@ if (count($row) > 0) {
 } else {
     echo json_encode(["operator" => "unavailable"]);
 }
-/* $statement = $pdo->query($sql);
-
-// get all publishers
-$publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-if ($publishers) {
-	// show the publishers
-	foreach ($publishers as $publisher) {
-		echo $publisher['name'] . '<br>';
-	}
-} */
-
-
-//$xx = array("a" => "Apple", "b" => "Ball", "c" => "Cat");
-/* 
-
-$content = trim(file_get_contents("php://input"));
-$data = json_decode($content, true);
-
-
-echo json_encode($data['operation'], true); */
-//echo json_encode($xx);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"],
+    "./src/**/*.{html,js}"
+    ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    
   ],
 }
